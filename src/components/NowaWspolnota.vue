d813c64a439c1c42063ac30072598b19dce73c64
Maciek Patrowicz <maciej.patrowicz@gmail.com>
2018-04-12 21:41:31 +0200

    wprowadzanie...

---
+++ src/components/NowaWspolnota.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-stepper v-model="e1" vertical>
+    <v-stepper-step step="1" :complete="e1 > 1">Wyszukaj siedzibę</v-stepper-step>
+    <v-stepper-content step="1">
+      <v-card color="grey lighten-1" class="mb-5">
+        <VueGoogleAutocomplete
+          id="map"
+          ref="address"
+          classname="form-control"
+          placeholder="Wpisz adres"
+          country="PL"
+          @placechanged="adressHasChanged"
+          types="(church)"
+        ></VueGoogleAutocomplete>
+
+        <v-btn/>
+      </v-card>
+
+      <v-btn color="primary" @click.native="e1 = 2">Continue</v-btn>
+      <v-btn flat>Cancel</v-btn>
+    </v-stepper-content>
+    <v-stepper-step step="2" :complete="e1 > 2">Name of step 2</v-stepper-step>
+    <v-stepper-content step="2">
+      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+      <v-btn color="primary" @click.native="e1 = 3">Continue</v-btn>
+      <v-btn flat>Cancel</v-btn>
+    </v-stepper-content>
+    <v-stepper-step step="3">Name of step 3</v-stepper-step>
+    <v-stepper-content step="3">
+      <v-card color="grey lighten-1" class="mb-5" height="200px"></v-card>
+      <v-btn color="primary" @click.native="e1 = 1">Continue</v-btn>
+      <v-btn flat>Cancel</v-btn>
+    </v-stepper-content>
+
+  </v-stepper>
+</template>
+
+<script>
+  import VueGoogleAutocomplete from 'vue-google-autocomplete'
+  export default {
+      name: "NowaWspolnota",
+      components: {
+        VueGoogleAutocomplete
+      },
+      data() {
+        return {
+          e1:"1"
+        }
+      },
+      methods: {
+        adressHasChanged(addr) {
+          console.log(addr)
+        },
+        myAddress() {
+          this.$refs.address.geolocate()
+        }
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Start from '@/components/Start'
+import NowaWspolnota from '@/components/NowaWspolnota'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Start',
       component: Start
+    },
+    {
+      path: '/nowa-wspolnota',
+      name: 'NowaWspolnota',
+      component: NowaWspolnota
     }
   ]
 })
